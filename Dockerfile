5cac0f2466cdfa85b7bb5567b302af871962e113
FBL <fabien.bardouil@gmail.com>
2026-01-05 11:42:35 +0100

    + Création de la structure de base du dossier projet

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+﻿FROM python:3.13-slim
+WORKDIR /app
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /bin/uv
+COPY pyproject.toml uv.lock ./
+RUN uv sync --frozen
+COPY . .
---
+++ config/config.py
@@ -0,0 +1,15 @@
+﻿import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+EXTERNAL_DATA_DIR = BASE_DIR / 'data' / 'external'
+RAW_DATA_DIR = BASE_DIR / 'data' / 'raw'
+PROCESSED_DATA_DIR = BASE_DIR / 'data' / 'processed'
+
+VIOLET_CLAIR = '#99abf7'
+VIOLET_FONCE = '#7451eb'
+GRIS_CLAIR = '#3f3f3f'
+JAUNE_FONCE = '#f9ab2d'
+JAUNE_CLAIR = '#f7e096ff'
+
+PALETTE = [VIOLET_CLAIR, VIOLET_FONCE, GRIS_CLAIR, JAUNE_FONCE, JAUNE_CLAIR]
